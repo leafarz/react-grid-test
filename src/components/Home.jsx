a628b0e9c3aa750b1263fbbfde5a5ac1504fd002
Raf Formes <rafformes@gmail.com>
2018-11-13 19:28:47 +0800

    added body contents

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// styled elements
+import Grid from './../elements/Grid';
+import g from './../js/global';
+
 // textures
 import img_headline from './../assets/textures/games01.jpg';
+import img_pic1 from './../assets/textures/games02.jpg';
+import img_pic2 from './../assets/textures/games03.jpg';
 
 const Home = () => {
   const ImageContainer = styled.div`
@@ -12,12 +18,56 @@ const Home = () => {
   const HeadlineImage = styled.img`
     width: 100%;
     margin:-20% 0;
+    filter: grayscale(50%);
+  `;
+  const HeadlineText = styled.div`
+    height: 200px;
+    width: 100%;
+    background-image: linear-gradient(rgb(30, 30, 30), rgb(25, 25, 25), rgb(10, 10, 10));
+    text-align: center;
+    color: white;
+    h1 {
+      position: relative;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  `;
+  const GridContainerContainer = styled.div`
+    background-color: ${g.BGColor2};
+  `;
+  const GridContainer = styled(Grid.Container)`
+    width: 80%;
+    margin: 0 auto;
+  `;
+  const GridImg = styled.img`
+    width: 100%;
+  `;
+  const GridItem = styled(Grid.Item)`
+    color: ${g.BodyFColor};
   `;
   return (
     <body>
-      <ImageContainer>
-        <HeadlineImage src={img_headline} alt=""/>
-      </ImageContainer>
+      <div>
+        <ImageContainer>
+          <HeadlineImage src={img_headline} alt="" />
+        </ImageContainer>
+        <HeadlineText>
+          <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor ullam corporis fugiat.</h1>
+        </HeadlineText>
+      </div>
+
+      <GridContainerContainer>
+        <GridContainer columns={2} gap={20}>
+          <GridItem>
+            <GridImg src={img_pic1} alt="" />
+          </GridItem>
+          <GridItem>asdf</GridItem>
+          <GridItem>asdf</GridItem>
+          <GridItem>
+            <GridImg src={img_pic2} alt="" />
+          </GridItem>
+        </GridContainer>
+      </GridContainerContainer>
     </body>
   )
 }
--- src/js/global.js
+++ src/js/global.js
@@ -1,9 +1,11 @@
 const Margin = 100;
 const BGColor1 = 'rgb(46, 46, 46)';
 const BGColor2 = 'rgb(10, 10, 10)';
+const BodyFColor = 'rgb(190, 190, 190)';
 
 export default {
   Margin,
   BGColor1,
-  BGColor2
+  BGColor2,
+  BodyFColor
 }
