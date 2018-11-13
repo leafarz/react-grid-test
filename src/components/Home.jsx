4e64f333b78b78a80377827c9d0fa6013e7a5147
Raf Formes <rafformes@gmail.com>
2018-11-13 18:52:27 +0800

    added headline image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+// libraries
+import React from 'react';
+import styled from 'styled-components';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        Home
-      </div>
-    )
-  }
+// textures
+import img_headline from './../assets/textures/games01.jpg';
+
+const Home = () => {
+  const ImageContainer = styled.div`
+    overflow:hidden;
+  `;
+  const HeadlineImage = styled.img`
+    width: 100%;
+    margin:-20% 0;
+  `;
+  return (
+    <body>
+      <ImageContainer>
+        <HeadlineImage src={img_headline} alt=""/>
+      </ImageContainer>
+    </body>
+  )
 }
+
+export default Home
